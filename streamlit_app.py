3c3c7e3234d5515508a45da95d6687e2eb43e603
bwbreeding-git <b.w.breeding@gmail.com>
2026-07-14 21:18:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import os
+import requests  
 
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -50,6 +51,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests  
+
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
